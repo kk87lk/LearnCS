7764aa08fc1ff3275b939534110d133bec5e32fd
kk87lk <786279458@qq.com>
2020-09-06 18:30:22 +0800

    First contact with c#

---
+++ 002-LearnCS/002-LearnCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _002_LearnCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
