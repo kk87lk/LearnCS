d519efb5c5906d2d12b19a1f441bae959af901cb
kk87lk <786279458@qq.com>
2020-09-11 23:37:29 +0800

    day 3

---
+++ 003-LearnCS/003-LearnCS/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _003_LearnCS
+{
+    class ClassA<T>
+    {
+        private T a;
+        public ClassA(T a)
+        {
+            this.a = a;
+        }
+        public void getA()
+        {
+            Console.WriteLine(a);
+            return;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scoreList = new List<int>();
+            scoreList.Add(11);
+            scoreList.Add(121);
+            scoreList.Add(15);
+            scoreList.Add(123);
+            scoreList.Add(52);
+            foreach(var i in scoreList){
+                Console.WriteLine(i);
+            }
+            var o1 = new ClassA<int>(12);
+            o1.getA();
+        }
+    }
+}
