5c176c36aa18e2db73d19ea8cf9b7677600c8a8b
kk87lk <786279458@qq.com>
2020-09-12 19:48:55 +0800

    day 04

---
+++ 004-LearnCS/004-LearnCS/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _004_LearnCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "www.11.com";
+            int length = s.Length;
+        }
+    }
+}
